59c4d197c6f734f97e9973395237f595c814839c
charl <charles.jacquin@autistici.org>
2014-10-17 16:24:17 +0200

    added allow-control-access-origin * in server.js

--- src/server.js
+++ src/server.js
@@ -11,10 +11,9 @@ app.set('port', process.env.PORT || 5010);
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "X-Requested-With");
-//        res.header('Access-Control-Allow-Origin', config.crossOrigin);
 //    res.header('Access-Control-Allow-Credentials',true);
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-//    res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
+    res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
     next();
 });
 
