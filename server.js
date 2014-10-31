2d1698450e2177a50075c94f792773b7a127b29e
charl <charles.jacquin@autistici.org>
2014-10-31 01:00:40 +0100

    deployment fixes

--- server.js
+++ server.js
@@ -5,7 +5,10 @@ Array.prototype.move = function(from, to) {
 require('./config/init')();
 var config = require('./config/config'),
     mongoose = require('mongoose-q')(),
-    db = mongoose.connect(config.dbUrl);
+    db = mongoose.connect(config.dbUrl,{
+        user : config.dbRoot,
+        pass : config.dbPassword
+    });
 var app = require('./config/express')(db);
 app.listen(config.port,config.address);
 console.log('CodeAssessments api started on port ' + config.port);
