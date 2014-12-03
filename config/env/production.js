e0fe1f1204d78421ce20fdd9c48e606c8fc9a89b
charl <charles.jacquin@autistici.org>
2014-12-03 16:18:19 +0100

    production example config file

--- config/env/production.js
+++ config/env/production.js
@@ -1,8 +1,8 @@
 module.exports = {
-  authUrl: 'learnerprofile-adapteach.rhcloud.com',
-  dbUrl: 'mongodb://' + process.env.OPENSHIFT_MONGODB_DB_HOST + ':' + process.env.OPENSHIFT_MONGODB_DB_PORT + '/codeassessments',
-  dbRoot: 'admin',
-  dbPassword: 'KAPDyGr4G2Rp',
-  port: process.env.OPENSHIFT_NODEJS_PORT || 8080,
-  address: process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1"
+  authUrl: '',
+  dbUrl: 'mongodb://...',
+  dbRoot: '',
+  dbPassword: '',
+  port: '',
+  address: ""
 };
