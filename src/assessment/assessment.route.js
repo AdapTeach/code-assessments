260c240ec9b55b7c7e6f72b87a9565b1746baa9b
Mikael Couzic <mikaelcouzic@gmail.com>
2014-12-08 11:47:44 +0100

    Populate tests before submitting

--- src/assessment/assessment.route.js
+++ src/assessment/assessment.route.js
@@ -7,16 +7,16 @@ var Assessment = require('./assessment.model'),
 module.exports = function (app) {
 
     app.route('/assessment')
-        .get(ensureAuthenticated,function(request,response){
-            Assessment.find({ creator : request.user._id })
+        .get(ensureAuthenticated, function (request, response) {
+            Assessment.find({creator: request.user._id})
                 .execQ()
-                .then(function sendResponse(assessments){
+                .then(function sendResponse(assessments) {
                     response.status(200).json(assessments);
                 })
                 .catch(HttpError.handle(response));
         })
-        .post(ensureAuthenticated,function(request,response){
-            console.log(request.user,request.body);
+        .post(ensureAuthenticated, function (request, response) {
+            console.log(request.user, request.body);
             var assess = new Assessment(request.body);
             assess.creator = request.user._id;
             assess
@@ -26,18 +26,18 @@ module.exports = function (app) {
                 })
                 .catch(HttpError.handle(response));
         })
-        .put(ensureAuthenticated,function(request,response){
+        .put(ensureAuthenticated, function (request, response) {
             var id = request.body._id;
             delete request.body._id;
             Assessment
                 .findOneAndUpdateQ({_id: id}, request.body)
                 .then(function sendResponse(assessment) {
-                    if(!assessment){
-                        HttpError.throw(400,"The assessment you're looking at doesn't exist.");
+                    if (!assessment) {
+                        HttpError.throw(400, "The assessment you're looking at doesn't exist.");
                     }
                     var data = {
-                        _id : assessment._id,
-                        title : assessment.title
+                        _id: assessment._id,
+                        title: assessment.title
                     };
                     response.json(data);
                 })
@@ -45,40 +45,39 @@ module.exports = function (app) {
         });
 
     app.route('/assessment/:id')
-        .get(ensureAuthenticated,function(request,response){
+        .get(ensureAuthenticated, function (request, response) {
             Assessment
                 .findOne({_id: request.params.id})
                 .populate('providedCompilationUnits compilationUnitsToSubmit tests guides')
                 .execQ()
                 .then(function sendResponse(assessment) {
-                    console.log(assessment)
-                    if(!assessment){
-                        HttpError.throw(400,"The assessment you're looking at doesn't exist.");
+                    if (!assessment) {
+                        HttpError.throw(400, "The assessment you're looking at doesn't exist.");
                     }
                     response.status(200).json(assessment);
                 }).catch(HttpError.handle(response));
         })
-        .put(ensureAuthenticated,function(request,response){
+        .put(ensureAuthenticated, function (request, response) {
             delete request.body._id;
             Assessment
                 .findOneAndUpdateQ({_id: request.params.id}, request.body)
                 .then(function sendResponse(assessment) {
-                    if(!assessment){
-                        HttpError.throw(400,"The assessment you're looking at doesn't exist.");
+                    if (!assessment) {
+                        HttpError.throw(400, "The assessment you're looking at doesn't exist.");
                     }
                     response.json(assessment);
                 })
                 .fail(HttpError.handle(response));
         })
-        .delete(ensureAuthenticated,function(request,response){
+        .delete(ensureAuthenticated, function (request, response) {
             Assessment
                 .remove({_id: request.params.id}).execQ()
                 .then(function sendResponse(nb) {
-                    if(nb === 0){
-                        HttpError.throw(400,"The assessment you're looking at doesn't exist.");
+                    if (nb === 0) {
+                        HttpError.throw(400, "The assessment you're looking at doesn't exist.");
                     }
                     response.status(200).json({
-                        message : 'assessment successfully removed'
+                        message: 'assessment successfully removed'
                     });
                 })
                 .fail(HttpError.handle(response));
@@ -86,17 +85,17 @@ module.exports = function (app) {
 
 
     //change the index of a guide in an assessment's array of guide
-    app.route(ensureAuthenticated,'/assessment/:id/guide/move/:from/to/:to')
-        .put(function(request,response){
+    app.route(ensureAuthenticated, '/assessment/:id/guide/move/:from/to/:to')
+        .put(function (request, response) {
             Assessment
                 .findOne({_id: request.params.id})
                 .execQ()
                 .then(function (assessment) {
                     return assessment.moveGuide(request.params.from, request.params.to);
                 })
-                .then(function(){
+                .then(function () {
                     response.status(200).json({
-                        message : 'success'
+                        message: 'success'
                     });
                 })
                 .catch(HttpError.handle(response));
@@ -107,14 +106,13 @@ module.exports = function (app) {
             .findOne({_id: request.params.id})
             .populate('compilationUnitsToSubmit')
             .populate('providedCompilationUnits')
+            .populate('tests')
             .execQ()
             .then(function (assessment) {
-                console.log(assessment);
-                console.log(request.body);
-                var submittedCompilationUnits = request.body.compilationUnits;
+                var submittedCompilationUnits = request.body.submittedCompilationUnits;
                 var options = {
-                    //url: 'http://localhost:5020/v1/',
-                    url: 'http://54.171.154.216:5020/v1/',
+                    url: 'http://localhost:5020/v1/',
+                    //url: 'http://54.171.154.216:5020/v1/',
                     method: 'POST',
                     body: [
                         JSON.stringify({
@@ -126,7 +124,6 @@ module.exports = function (app) {
                 return http.request(http.normalizeRequest(options))
                     .then(function (submissionResponse) {
                         return submissionResponse.body.read().then(function (body) {
-                            console.log(body);
                             response
                                 .status(submissionResponse.status)
                                 .json(JSON.parse(body));
